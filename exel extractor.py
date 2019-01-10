4a7f5989cb6816d3be016b56728f40aa904dec30
Mohsencactus <mohsennasiri8@gmail.com>
2019-01-10 21:06:50 +0330

    done

--- exel extractor.py
+++ exel extractor.py
@@ -1,34 +1,14 @@
-'''
 import xlrd  
-loc = ("Book1.xlsx") 
-wb = xlrd.open_workbook(loca) 
-sheet = wb.sheet_by_index(0) 
-# For row 0 and column 0 
-sheet.cell_value(0, 0) 
+####################################
+adress = ("Book1.xlsx")# masir file excel 
 
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
-sheet.cell_value(0, 0) 
-  
-#shomare row va shomare column
-print(sheet.nrows)
-print(sheet.ncols)
-for i in range(sheet.ncols): 
-    print(sheet.cell_value(0, i))
-for i in range(sheet.nrows): 
-    print(sheet.cell_value(i, 0)) 
-print(sheet.row_values(1)) 
-'''
-import xlrd  
-loc = ("Book1.xlsx") 
+wb = xlrd.open_workbook(adress)# baray tabdil kardan file excel be type:xlrdbook
+sheet = wb.sheet_by_index(0) # baray tabdil kardan file excel be type:xlrdsheet
 
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
-sheet.cell_value(0, 0) 
+dynamic = [] # sakhte ye array ke meghdar haro brizim tosh
 
-dynamic = []
-for i in range(sheet.ncols):
-    arv = sheet.cell_value(0, i)
-    dynamic.append(arv)
-    print(sheet.cell_value(0, i))
-print(dynamic)
+for i in range(sheet.ncols): # baraye ejraye halghe be andaze sutun
+    print(sheet.cell_value(0, i)) # print kardan meghdar har sutun
+    arv = sheet.cell_value(0, i) # rikhtan meghdar har sutun toye in moteghayer
+    dynamic.append(arv) # ezafe kardan meghdar har sutun be arayeye dynamic
+print(dynamic)# namayesh arrayeye dynamic ke meghdar har sutun ra darad
--- exel reader stuff(copy).py
+++ exel reader stuff(copy).py
@@ -1,34 +1,18 @@
-'''
-import xlrd  
-loc = ("Book1.xlsx") 
-wb = xlrd.open_workbook(loca) 
-sheet = wb.sheet_by_index(0) 
-# For row 0 and column 0 
-sheet.cell_value(0, 0) 
+import xlrd  # ketabkhune marbut 
 
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
-sheet.cell_value(0, 0) 
-  
-#shomare row va shomare column
-print(sheet.nrows)
-print(sheet.ncols)
-for i in range(sheet.ncols): 
-    print(sheet.cell_value(0, i))
-for i in range(sheet.nrows): 
-    print(sheet.cell_value(i, 0)) 
-print(sheet.row_values(1)) 
-'''
-import xlrd  
-loc = ("Book1.xlsx") 
 
-wb = xlrd.open_workbook(loc) 
-sheet = wb.sheet_by_index(0) 
-sheet.cell_value(0, 0) 
+adress = ("Book1.xlsx") # mahal fiel excel  
 
-dynamic = []
-for i in range(sheet.ncols):
-    arv = sheet.cell_value(0, i)
-    dynamic.append(arv)
-    print(sheet.cell_value(0, i))
-print(dynamic)
+wb = xlrd.open_workbook(adress)# baray tabdil kardan file excel be type:xlrdbook
+sheet = wb.sheet_by_index(0) # baray tabdil kardan file excel be type:xlrdsheet
+
+sheet.cell_value(0, 0) # baraye daryafte meghdar satr0 , sutun0 
+
+print(sheet.nrows) # baraye namayesh tedad satr
+print(sheet.ncols) # baraye namayesh tedad sutun
+
+for i in range(sheet.ncols): # ejraye halghe be tedad sutun
+    print(sheet.cell_value(0, i))# namayesh meghdar har sutun
+
+for i in range(sheet.nrows): # ejraye halghe be tedad satr
+    print(sheet.cell_value(i, 0)) # namayesh meghdar har satr
